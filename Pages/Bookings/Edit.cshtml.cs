c7cf513a365d8a02400618eef83c3ae95edfffc1
Jorge Dionisio <L00177788@atu.ie>
2023-03-13 22:29:35 +0000

    Final version

--- Pages/Bookings/Edit.cshtml.cs
+++ Pages/Bookings/Edit.cshtml.cs
@@ -104,16 +104,155 @@ namespace GolfClub.Pages.Bookings
             {
                 return Page();
             }
+            var originalBooking = _context.Booking.AsNoTracking().FirstOrDefault(b => b.BookingId == Booking.BookingId);
             
+            var bookingsForGivenDay = _context.Booking.AsNoTracking().Where(b => b.TeeTime.Date == Booking.TeeTime.Date);
+            bool invalidBookingPlayerOne = false;
+            bool invalidBookingPlayerTwo = false;
+            bool invalidBookingPlayerThree = false;
+            bool invalidBookingPlayerFour = false;
+            foreach (var booking in bookingsForGivenDay)
+            {
+                if (originalBooking.PlayerOneId != Booking.PlayerOneId)
+                {
+                    if (Booking.PlayerOneId == booking.PlayerOneId)
+                    {
+                        invalidBookingPlayerOne = true;
+                    }
+                    if (Booking.PlayerOneId == booking.PlayerTwoId)
+                    {
+                        invalidBookingPlayerOne = true;
+                    }
+                    if (Booking.PlayerOneId == booking.PlayerThreeId)
+                    {
+                        invalidBookingPlayerOne = true;
+                    }
+                    if (Booking.PlayerOneId == booking.PlayerFourId)
+                    {
+                        invalidBookingPlayerOne = true;
+                    }
+                }
+                if (originalBooking.PlayerTwoId != Booking.PlayerTwoId && Booking.PlayerTwoId != 0) { 
+                    if (Booking.PlayerTwoId == booking.PlayerOneId)
+                    {
+                        invalidBookingPlayerTwo = true;
+                    }
+                    if (Booking.PlayerTwoId == booking.PlayerTwoId)
+                    {
+                        invalidBookingPlayerTwo = true;
+                    }
+                    if (Booking.PlayerTwoId == booking.PlayerThreeId)
+                    {
+                        invalidBookingPlayerTwo = true;
+                    }
+                    if (Booking.PlayerTwoId == booking.PlayerFourId)
+                    {
+                        invalidBookingPlayerTwo = true;
+                    }
+                }
+                if (originalBooking.PlayerThreeId != Booking.PlayerThreeId && Booking.PlayerThreeId != 0)
+                { 
+                    if (Booking.PlayerThreeId == booking.PlayerOneId)
+                    {
+                        invalidBookingPlayerThree = true;
+                    }
+                    if (Booking.PlayerThreeId == booking.PlayerTwoId)
+                    {
+                        invalidBookingPlayerThree = true;
+                    }
+                    if (Booking.PlayerThreeId == booking.PlayerThreeId)
+                    {
+                        invalidBookingPlayerThree = true;
+                    }
+                    if (Booking.PlayerThreeId == booking.PlayerFourId)
+                    {
+                        invalidBookingPlayerThree = true;
+                    }
+                }
+                if (originalBooking.PlayerFourId != Booking.PlayerFourId && Booking.PlayerFourId != 0)
+                    {
+                    if (Booking.PlayerFourId == booking.PlayerOneId)
+                    {
+                        invalidBookingPlayerFour = true;
+                    }
+                    if (Booking.PlayerFourId == booking.PlayerTwoId)
+                    {
+                        invalidBookingPlayerFour = true;
+                    }
+                    if (Booking.PlayerFourId == booking.PlayerThreeId)
+                    {
+                        invalidBookingPlayerFour = true;
+                    }
+                    if (Booking.PlayerThreeId == booking.PlayerFourId)
+                    {
+                        invalidBookingPlayerFour = true;
+                    }
+                }
+            }
             _context.Attach(Booking).State = EntityState.Modified;
-            Booking.PlayerOne = _context.Member.FirstOrDefault(x => x.MembershipNumberId == Booking.PlayerOneId);
-            Booking.PlayerOneId = Booking.PlayerOneId;
-            Booking.PlayerTwo = null;
-            Booking.PlayerTwoId = null;
-            Booking.PlayerThreeId = null;
-            Booking.PlayerThree = null;
-            Booking.PlayerFour = null;
-            Booking.PlayerFourId = null;
+            Booking.PlayerOne = _context.Member.FirstOrDefault(m => m.MembershipNumberId == Booking.PlayerOneId);
+            if (Booking.PlayerTwoId != 0)
+            {
+                Booking.PlayerTwo = _context.Member.FirstOrDefault(m => m.MembershipNumberId == Booking.PlayerTwoId);
+            }
+            if (Booking.PlayerThreeId != 0)
+            {
+                Booking.PlayerThree = _context.Member.FirstOrDefault(m => m.MembershipNumberId == Booking.PlayerThreeId);
+            }
+            if (Booking.PlayerFourId != 0)
+            {
+                Booking.PlayerFour = _context.Member.FirstOrDefault(m => m.MembershipNumberId == Booking.PlayerFourId);
+            }
+            if (invalidBookingPlayerOne)
+            {
+                ViewData["ErrorMessage"] += " Player Name: " + Booking.PlayerOne.Name + " already has a booking for that day";
+            }
+            if (invalidBookingPlayerTwo)
+            {
+                ViewData["ErrorMessage"] += " Player Name: " + Booking.PlayerTwo.Name + " already has a booking for that day";
+            }
+            if (invalidBookingPlayerThree)
+            {
+                ViewData["ErrorMessage"] += " Player Name: " + Booking.PlayerThree.Name + " already has a booking for that day";
+            }
+            if (invalidBookingPlayerFour)
+            {
+                ViewData["ErrorMessage"] += " Player Name: " + Booking.PlayerFour.Name + " already has a booking for that day";
+            }
+
+            if (invalidBookingPlayerOne || invalidBookingPlayerTwo || invalidBookingPlayerThree || invalidBookingPlayerFour)
+            {
+                ViewData["PlayerOneId"] = new SelectList(_context.Set<Member>(), "MembershipNumberId", "Name");
+                var emptyMember = new Member { MembershipNumberId = 0, Name = "Optional" };
+
+                var selectListOptionalList = new List<Member>
+                {
+                    emptyMember
+                };
+                foreach (var item in _context.Set<Member>())
+                {
+                    selectListOptionalList.Add(item);
+                }
+                ViewData["PlayerTwoId"] = new SelectList(selectListOptionalList, "MembershipNumberId", "Name");
+                ViewData["PlayerThreeId"] = new SelectList(selectListOptionalList, "MembershipNumberId", "Name");
+                ViewData["PlayerFourId"] = new SelectList(selectListOptionalList, "MembershipNumberId", "Name");
+                return Page();
+            }
+            if (Booking.PlayerTwoId == 0)
+            {
+                Booking.PlayerTwo = null;
+                Booking.PlayerTwoId = null;
+            }
+            if (Booking.PlayerThreeId == 0)
+            {
+                Booking.PlayerThree = null;
+                Booking.PlayerThreeId = null;
+            }
+            if (Booking.PlayerFourId == 0)
+            {
+                Booking.PlayerFour = null;
+                Booking.PlayerFourId = null;
+            }
             try
             {
                 await _context.SaveChangesAsync();
